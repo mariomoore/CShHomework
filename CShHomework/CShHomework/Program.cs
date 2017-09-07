063e2b7a13a3e8eb2dd174397cbf64e89749a21d
Mariusz Murawski <mariusz.murawski84@gmail.com>
2017-09-07 19:11:07 +0200

    Zadanie łatwe. Program sumuje parzyste liczby z tablicy 50 losowych liczb.

--- CShHomework/CShHomework/Program.cs
+++ CShHomework/CShHomework/Program.cs
@@ -8,8 +8,31 @@ namespace CShHomework
 {
     class Program
     {
+        public static void sumaLosowych()
+        {
+            int[] tablicaStartowa = new int[1000];
+            int[] tablica = new int[50];
+            Random los = new Random();
+            int suma = 0;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                tablicaStartowa[i] = los.Next(0, 1001);
+            }
+
+            for (int i = 0; i < 50; i++)
+            {
+                tablica[i] = tablicaStartowa[los.Next(0, 999)];
+                if (tablica[i] % 2 == 0) suma += tablica[i];
+            }
+
+            Console.WriteLine("Suma wylosowanych liczb to " + suma);
+            Console.ReadKey();
+        }
+
         static void Main(string[] args)
         {
+            sumaLosowych();
         }
     }
 }
