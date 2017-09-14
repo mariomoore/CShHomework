8bc7683e02eea7bac75a2564dbe7d4bbf8c1140d
Mariusz Murawski <mariusz.murawski84@gmail.com>
2017-09-14 19:58:31 +0200

    Program wyłapuje wyjątek, gdy użytkownik wpisze tekst większy niż 50 znaków.

--- CShHomework/CShHomework/Program.cs
+++ CShHomework/CShHomework/Program.cs
@@ -6,10 +6,43 @@ using System.Threading.Tasks;
 
 namespace CShHomework
 {
+    class MoreThanFiftyCharsException : Exception
+    {
+        public MoreThanFiftyCharsException() : base() { }
+
+        public MoreThanFiftyCharsException(string message) : base(message) { }
+
+        public MoreThanFiftyCharsException(string format, params object[] args) : base(string.Format(format, args)) { }
+
+        public MoreThanFiftyCharsException(string message, Exception innerException) : base(message, innerException) { }
+    }
+
     class Program
     {
+        public static void LapWyjatek()
+        {
+            string tekst = "";
+
+            Console.Write("Wpisz tekst: ");
+
+            try
+            {
+                tekst = Console.ReadLine();
+                if (tekst.Length > 50) throw new MoreThanFiftyCharsException("Za długi ciąg wejściowwy");
+            }
+            catch (MoreThanFiftyCharsException ex)
+            {
+                Console.WriteLine("Wyłapano wyjątek: " + ex.Message + ". Tekst został skrócony do 50 znaków.");
+                tekst = tekst.Substring(0, 50);
+            }
+
+            Console.WriteLine(tekst);
+            Console.ReadKey();
+        }
+
         static void Main(string[] args)
         {
+            LapWyjatek();
         }
     }
 }
