c23de9b4448fc47ab80b165e5e85b90035cc6fd9
Mariusz Murawski <mariusz.murawski84@gmail.com>
2017-09-14 23:37:49 +0200

    Program implementuje interface IOsoba w klasie Student, a następnie sortuje listę studentów w ArrayList.

--- CShHomework/CShHomework/Program.cs
+++ CShHomework/CShHomework/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,73 @@ using System.Threading.Tasks;
 
 namespace CShHomework
 {
+    interface IOsoba
+    {
+        string Nazwa { get; }
+        void Opisz();
+    }
+
+    class Student : IOsoba
+    {
+        private string nazwa;
+        public string Nazwa { get { return nazwa; } }
+
+        public Student()
+        {
+            nazwa = "Nieznany student";
+        }
+
+        public Student(string nazwa)
+        {
+            this.nazwa = nazwa;
+        }
+        
+        public void Opisz()
+        {
+            Console.WriteLine("Student: " + nazwa);
+        }
+    }
+
+    public class StudentSort : IComparer
+    {
+        int IComparer.Compare(Object x, Object y)
+        {
+            Student s1 = x as Student;
+            Student s2 = y as Student;
+            return ((new CaseInsensitiveComparer()).Compare(s1.Nazwa, s2.Nazwa));
+        }
+    }
+
     class Program
     {
+        public static void SortowanieStudentow()
+        {
+            ArrayList studenty = new ArrayList();
+            studenty.Add(new Student());
+            studenty.Add(new Student("Mariusz Murawski"));
+            studenty.Add(new Student("Igrek Iksiński"));
+            studenty.Add(new Student("Iks Igrekowski"));
+
+            Console.WriteLine("Lista przed sortowaniem: ");
+            foreach (Student s in studenty)
+            {
+                s.Opisz();
+            }
+
+            studenty.Sort(new StudentSort());
+
+            Console.WriteLine("\n\rLista po sortowaniu:");
+            foreach (Student s in studenty)
+            {
+                s.Opisz();
+            }
+
+            Console.ReadKey();
+        }
+
         static void Main(string[] args)
         {
+            SortowanieStudentow();
         }
     }
 }
